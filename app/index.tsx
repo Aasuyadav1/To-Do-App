769614391eeb92da03f74d6b08510cb120310820
aasuyadav1 <aasuy284@gmail.com>
2024-11-04 01:23:17 +0530

    fix: tailwind css

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='text-3xl font-bold bg-red-400 text-yellow-100'>index</Text>
+    </View>
+  )
+}
+
+export default index
